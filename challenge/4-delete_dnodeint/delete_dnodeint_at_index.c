72ac8e828bfa681609d3ac54876d6f745068ef32
AxelNAY <axelnay@sfr.fr>
2024-09-10 09:59:18 +0200

    Fix task 4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,44 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
 	unsigned int p;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 	{
 		return (-1);
 	}
-	current = *head;
-
-	if (0 == index)
-	{
-		*head = current->next;
-		if (*head != NULL)
-		{
-			(*head)->prev = NULL;
-		}
-		free(current);
-		return	(1);
-	}
-
+	saved_head = *head;
 	p = 0;
-	while (p < index && current != NULL)
+	while (p < index && *head != NULL)
 	{
 		*head = (*head)->next;
 		p++;
 	}
-	if (current == NULL)
+	if (p != index)
 	{
+		*head = saved_head;
 		return (-1);
 	}
-	if (current->next != NULL)
+	if (0 == index)
 	{
-		current->next->prev = current->prev;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
 	}
-	if (current->prev != NULL)
+	else
 	{
-		current->prev->next = current->next;
+		(*head)->prev->prev = (*head)->prev;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
 	}
-	free(current);
 	return (1);
 }
